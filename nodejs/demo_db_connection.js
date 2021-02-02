f96dee13dcd1396b0f6bbd915dcaea47134669ad
julien641 <julien641@yahoo.com>
2021-02-02 17:41:16 -0500

    Update demo_db_connection.js

--- nodejs/demo_db_connection.js
+++ nodejs/demo_db_connection.js
@@ -1,9 +1,9 @@
 sql = require('mysql');
 
 var con = sql.createConnection({
-  host: "192.155.88.98",
+  host: "localhost",
   user: "web",
-  password: "930492hudfalknsd32048"
+  password: "password"
 });
 
 con.connect(function(err) {
